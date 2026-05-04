8cf78d3f1b99fbf6f444e80fb8b3f6ce91153cbf
choco4147 <100542480+choco4147@users.noreply.github.com>
2026-05-04 03:09:22 -0500

    added a bunch of code

--- Electronics/ArduinoCode/ArduinoCode.ino
+++ Electronics/ArduinoCode/ArduinoCode.ino
@@ -10,7 +10,7 @@ Servo srv;
 
 Adafruit_BMP3XX bmp;
 
-
+int startTime = 0;
 
 
 
@@ -28,8 +28,7 @@ void setup() {
 
   // define pins
   pinMode(13,OUTPUT); // watchdog LED
-  pinMode(11, INPUT); // button
-  pinMode(10,OUTPUT);
+  pinMode(11, INPUT); // button 
 }
 
 
@@ -41,8 +40,8 @@ void loop() {
   float asl = getA(); // launch site alt asl
   float maxA = 0; // saves flight max alt agl
   float maxS = 0; // saves flight max speed
-  int maxServo = 90; // max servo angle
-  int maxSpeed = 800; // max speed diff pressure sensor works at
+  int maxServo = 70; // max servo angle
+  int maxSpeed = 600; // max speed diff pressure sensor works at
   
   // state functions
   pad(maxServo);
@@ -76,8 +75,25 @@ float getT() {
 
 // gets altitude asl (ft)
 float getA() {
+  int t = millis() - startTime;
+  // TEST MODE FOR ROBOT SHOW
+  bool testing = true;
+
+  if (testing == false) {
   bmp.performReading();
   return bmp.readAltitude(1013.25) / 3.281;
+  }
+  else {
+    if(t<13000){ // flight
+      return (sq(t-10800)/-33000)+3800;
+    }
+    if(13000<t && t<25600){ // parachute
+      return 0.28*((t*-1)+13000)+3662;
+    }
+    if(25600<t) { // ground
+      return 136;
+    }
+  }
 }
 
 
@@ -104,8 +120,32 @@ float getPD() {
 
 // calculates inducated airspeed
 float getIAS() {
+  int t = millis() - startTime;
+  // TESTING MODE FOR ROBOT SHOW
+  bool testing = true;
+  float ias;
+  if (testing == false) {
   float d = 0.0765;
-  float ias = sqrt((2 * 32.174 * getPD() * 144) / d);
+   ias = sqrt((2 * 32.174 * getPD() * 144) / d);
+  }
+  else{
+    if(t<1500){ // boost
+      Serial.print("boost");
+      ias = sin(3.141592*t/3000)*575;
+    }
+    else if(1500<t && t<13000){ // coast
+      Serial.print("coast");
+      ias = 575-((1/13.2)*(t-1500));
+    }
+    else if(13000<t && t<25600) { // parachute
+      Serial.print("chute");
+      ias = -300;
+    }
+    else if(25600<t) { // ground
+      Serial.print("ground");
+      ias = 0;
+    }
+  }
   return ias;
 }
 
@@ -126,12 +166,18 @@ void pad(int maxServo) {
     digitalWrite(13,LOW);
     delay(750);
   }
+
   
   // armed, waiting on launch detect
+
   digitalWrite(13,LOW);
+  delay(100);
   srv.write(maxServo);
-  delay(350);
+  delay(1000);
   srv.write(0);
+  startTime = millis();
+  Serial.print(millis());
+  Serial.print(startTime);
   bool launched = false;
   int vote = 0;
   float pA = getA();
@@ -140,7 +186,8 @@ void pad(int maxServo) {
     
     // launch detect vote
     if(getA() > pA || getIAS() > pS) {vote = vote + 1;}
-    else{vote = 0;}
+    else if (vote > 0) {vote =vote - 1;}
+    else{ vote = 0;}
     if(vote >= 10){launched=true;}
     pA = getA();
     pS = getIAS();
@@ -169,17 +216,24 @@ void flying(float asl, int maxServo, int maxSpeed) {
     // update max speed and altitude
     if(getA()-asl > maxA) {maxA = getA()-asl;}
     if(getIAS() > maxS) {maxS = getIAS();}
-
     // landing detect vote
-    if (abs(getIAS()) < 5 || abs(pA-getA()) < 5) {vote = vote+1;}
+    if (abs(getIAS()) < 5 || abs(pA-getA()) < 0.5) {vote = vote+1;}
     else {vote = 0;}
     if (vote >= 10) {flying = false;}
-
+    Serial.print(getA());
+    Serial.print(" ");
+    Serial.println(pA);
     // servo code - currently set to get constant acceleration
     // based on assumption that force of drag scales with v^2
-    float servoState = ((float)(maxSpeed^2)-(getIAS()^2))/((float)maxSpeed^2);
-    srv.write(servoState*maxServo);
-
+    if(getIAS()>0){
+      srv.write(maxServo);
+      Serial.println("flying");
+    }
+    else{
+      srv.write(maxServo*0.5);
+      Serial.println("descending");
+    }
+    pA = getA();
     delay(50);
   }
 }
@@ -193,5 +247,13 @@ void flying(float asl, int maxServo, int maxSpeed) {
 
 // code for when the rocket has landed on the ground
 void landed(int maxS, int maxA){
+  srv.write(0);
   
+  while(1==1){
+    Serial.print("landed!");
+    digitalWrite(13, HIGH);
+    delay(250);
+    digitalWrite(13, LOW);
+    delay(750);
+  }
 }
